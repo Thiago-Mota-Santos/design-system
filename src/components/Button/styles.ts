8222369f2cf038611090c8d73be87703f2094d16
Thiago Mota Santos <lucimara1422@gmail.com>
2023-01-09 16:46:38 -0300

    added script

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components/macro';
+import styled from 'styled-components';
 import { ButtonProps } from './index';
 
 export const Container = styled.button<ButtonProps>`
